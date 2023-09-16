f01fb321347adeb98dc2120240ac118bb7e8255f
Manuel Gysin <manuel.gysin@amanda-technology.ch>
2023-09-16 14:38:44 +0200

    - Better logging with serilog - Parse datetime from output and set next retry based on it

--- GooglePhotoSyncCustom/Program.cs
+++ GooglePhotoSyncCustom/Program.cs
@@ -4,16 +4,28 @@ using CasCap.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Net;
+using System.Text.RegularExpressions;
+using Serilog;
+using Serilog.Context;
 
 // Logging stuff
-var loggerFactory = LoggerFactory.Create(builder => { builder.AddSimpleConsole( options => options.TimestampFormat = "[yyyy-MM-dd HH:mm:ss] "); });
+var serilog =  new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(outputTemplate:"[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level:u3}] {Method} - {Message:l}{NewLine}{Exception}")
+    .CreateLogger();
+
+var loggerFactory = new LoggerFactory().AddSerilog(serilog);
 
 // Run
 Task.WaitAll(SyncPhotos(loggerFactory), SendPostcard(loggerFactory));
+return;
 
 async Task SyncPhotos(ILoggerFactory factory)
 {
-    var logger = factory.CreateLogger<GooglePhotosService>();
+    LogContext.PushProperty("Method", nameof(SyncPhotos));
+    var logger = factory.CreateLogger<Program>();
+    var loggerGoogleSvc = factory.CreateLogger<GooglePhotosService>();
 
     // Envs
     var envUser = Environment.GetEnvironmentVariable("GPSC_USER");
@@ -80,7 +92,7 @@ async Task SyncPhotos(ILoggerFactory factory)
     var client = new HttpClient(handler) {BaseAddress = new Uri(options.BaseAddress)};
 
     // Google photo service
-    var googlePhotosSvc = new GooglePhotosService(logger, Options.Create(options), client);
+    var googlePhotosSvc = new GooglePhotosService(loggerGoogleSvc, Options.Create(options), client);
 
     // Try to login
     do
@@ -135,8 +147,10 @@ async Task SyncPhotos(ILoggerFactory factory)
 
 async Task SendPostcard(ILoggerFactory factory)
 {
-    var logger = factory.CreateLogger<GooglePhotosService>();
+    LogContext.PushProperty("Method", nameof(SendPostcard));
+    var logger = factory.CreateLogger<Program>();
     var envMediaFolderPath = Environment.GetEnvironmentVariable("GPSC_MEDIAFOLDERPATH");
+    var timeToDelayNextTry = (int) TimeSpan.FromMinutes(24 * 60 + 1).TotalMilliseconds;
 
     // Validate all envs are present
     if (string.IsNullOrWhiteSpace(envMediaFolderPath))
@@ -175,19 +189,50 @@ async Task SendPostcard(ILoggerFactory factory)
             if (proc.ExitCode != 0)
             {
                 logger.LogError("Send card failed!");
+
+                // try to parse output for next allowed send date
+                var regexResult = RegexDateTimeExtractor.DateTimeRegex().Match(output);
+
+                if (regexResult.Success)
+                {
+                    if (DateTimeOffset.TryParse(regexResult.Value, out var dateTimeOffset))
+                    {
+                        var currentTimeOffset = DateTimeOffset.Now;
+                        var delay = dateTimeOffset - currentTimeOffset;
+
+                        timeToDelayNextTry = (int) delay.TotalMilliseconds;
+
+                        logger.LogInformation("Found next allowed to send date, setting it to {Date}", dateTimeOffset.DateTime);
+                    }
+                    else
+                    {
+                        logger.LogWarning("No valid date found in output, waiting 1 hours for next retry");
+                        timeToDelayNextTry = (int) TimeSpan.FromHours(1).TotalMilliseconds;
+                    }
+                }
+                else
+                {
+                    logger.LogError("Unknown error, waiting 1 hours for next retry");
+                    timeToDelayNextTry = (int) TimeSpan.FromHours(1).TotalMilliseconds;
+                }
             }
             else
             {
                 logger.LogInformation("Card was sent successfully");
                 File.Delete(Path.Combine(envMediaFolderPath, fileToSend));
             }
-
-            await Task.Delay((int) TimeSpan.FromMinutes(24 * 60 + 1).TotalMilliseconds); // Add one minute
         }
         catch (Exception e)
         {
             logger.LogInformation("Send card failed with error: {Error}", e);
-            await Task.Delay((int) TimeSpan.FromMinutes(10).TotalMilliseconds); // In case of error
+            timeToDelayNextTry = (int) TimeSpan.FromMinutes(10).TotalMilliseconds;
         }
+
+        await Task.Delay(timeToDelayNextTry); // In case of error
     } while (true);
+}
+
+public static partial class RegexDateTimeExtractor{
+    [GeneratedRegex(@"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+-]\d{2}:\d{2}")]
+    public static partial Regex DateTimeRegex();
 }
